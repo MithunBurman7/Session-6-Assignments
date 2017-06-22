5ee56e7676470b1d29708cf9ec60517c25da9d51
MithunBurman7 <mithunburman7@gmail.com>
2017-06-22 15:22:01 +0530

    Session6- Assionment-1

---
+++ src/Assignments/ClassOne.java
@@ -0,0 +1,31 @@
+package Assignments;
+
+class ClassOne {
+
+	public static void main(String[] args) {// Main method
+		System.out.println("Class One - Hello Java"); 
+	}
+
+}
+class Two { 
+	
+	
+	public static void main(String[]args){// Main Class 
+		
+		System.out.println("Class Two - Welcome to Java");
+	}
+}
+class Three { 
+	public static void main(String[]args){// Main Class
+		
+		System.out.println("Class Three - Coding in Java"); 
+	}
+}
+class Fourth { 
+	public static void main(String[]args){// Main Class
+		
+		System.out.println("Class Fourth - Java"); 
+	}
+
+
+	}
